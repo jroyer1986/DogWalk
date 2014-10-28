cef50f561ff9193e3360ae55c572939ea768804d
Justin <jroyer1986@gmail.com>
2014-10-27 22:36:00 -0400

    created MessageRepo

---
+++ DogWalk.Data/Models/MessageModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogWalk.Data.Models
+{
+    public class MessageModel
+    {
+        #region Properties
+
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public DateTime DateOfMessage { get; set; } 
+
+        #endregion
+
+        #region Constructor
+
+        public MessageModel() { }
+
+        public MessageModel(int id, string message, DateTime dateOfMessage)
+        {
+            ID = id;
+            Message = message;
+            DateOfMessage = dateOfMessage;
+        }
+
+        #endregion
+    }
+}
---
+++ DogWalk.Data/Repositories/MessageRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogWalk.Data.Repositories
+{
+    class MessageRepository
+    {
+        public void SendMessage() { }
+
+        public void GetMessages() { }
+
+        public void GetMessageByID() { }    
+    }
+}
